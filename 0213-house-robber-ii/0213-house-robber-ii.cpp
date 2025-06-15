c439042b116a9f04dcd84efd31e7b472db4e84e9
Sujal Thapa <138743241+sujalthapa369@users.noreply.github.com>
2025-06-16 02:28:01 +0530

    Time: 0 ms (100%), Space: 10.3 MB (76.83%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int robLinear(vector<int>& nums, int start, int end) {
-        int prev = 0, curr = 0;
+    int t[101];
 
-        for (int i = start; i <= end; i++) {
-            int temp = max(curr, prev + nums[i]);
-            prev = curr;
-            curr = temp;
-        }
+    int solve(vector<int>& nums, int end, int idx) {
+        if (idx > end) return 0;
+        if (t[idx] != -1) return t[idx];
 
-        return curr;
+        int steal = nums[idx] + solve(nums, end, idx + 2);
+        int skip = solve(nums, end, idx + 1);
+
+        return t[idx] = max(steal, skip);
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
         if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
 
-        int option1 = robLinear(nums, 0, n - 2);
+        memset(t, -1, sizeof(t));
+        int takeFrom0 = solve(nums, n - 2, 0);
 
-        int option2 = robLinear(nums, 1, n - 1);
+        memset(t, -1, sizeof(t));
+        int takeFrom1 = solve(nums, n - 1, 1);
 
-        return max(option1, option2);
+        return max(takeFrom0, takeFrom1);
     }
 };
