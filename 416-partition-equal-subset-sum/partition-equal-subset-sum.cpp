b63994d7843159b29da58d39dfcc63c34ac7af64
Sujal Thapa <138743241+sujalthapa369@users.noreply.github.com>
2025-07-01 20:31:34 +0530

    Time: 67 ms (86.06%) | Memory: 15.4 MB (76.77%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool knapsack(vector<int>& arr, int sum, int n) {
+        bool t[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (i == 0)
+                    t[i][j] = false;
+                if (j == 0)
+                    t[i][j] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        return knapsack(nums, sum / 2, n);
+    }
+};
